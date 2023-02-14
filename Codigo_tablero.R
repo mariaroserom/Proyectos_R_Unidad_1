9bfc2218617f102b8f2be8db829e3e6e799a45b5
mariaroserom <mariaroseromontilla@javerianacali.edu.co>
2023-02-13 21:46:46 -0500

    first commit

---
+++ Codigo_tablero.R
@@ -0,0 +1,10 @@
+install.packages("flexdashboard")  # instala paquete flexdashboard
+install.packages("devtools")       # instala paquete devtools
+devtools::install_github("dgonxalex80/paqueteMET") # descarga de paqueteMET
+library(paqueteMET)                # activar paqueteMET
+
+data("CarreraLuz22")               # cargar la dataset
+
+install.packages("remotes")  
+remotes::install_github("rstudio/bslib")  # descarga paquete que contiene formato    
+
